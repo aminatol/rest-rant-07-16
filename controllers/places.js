e6c7d3e68c2caee9f4ca16325afe44d27d89d0ac
aminatol <aminatoluwo@gmail.com>
2023-07-24 20:40:40 -0400

    added inputs, created comments, saved comment

--- controllers/places.js
+++ controllers/places.js
@@ -55,7 +55,28 @@ router.get("/:id/edit", (req, res) => {
 });
 
 router.post("/:id/rant", (req, res) => {
-  res.send("GET /places/:id/rant stub");
+  console.log(req.body)
+  req.body.rant= req.body.rant ? true : false
+  db.Place.findById(req.params.id)
+  .then(place=>{
+    //Todo: Creat Comment
+    db.Comment.create(req.body)
+    .then(comment=>{
+      //Todo: Save comment id to place
+      place.comments.push(comment.id)
+      place.save()
+      .then(()=>{
+        res.redirect(`/places/${req.params.id}`)
+      })
+    })
+    .catch(err=>{
+      res.render("error404")
+    })
+  })
+  .catch(err=>{
+    res.render('error404')
+  })
+
 });
 
 router.delete("/:id/rant/:rantId", (req, res) => {
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -19,36 +19,34 @@ function show (data) {
       )
     });
   }
-    return ( 
+    return (
       <Def>
-  <main>
-    <div className="row">
-      <div className="col-sm-6">
-        <img src={data.place.pic} alt={data.place.name} />
-        <h3>
-          Located in {data.place.city}, {data.place.state}
-        </h3>
-      </div>
-      <div className="col-sm-6">
-        </div>
-        <h2>
-          Description
-        </h2>
-        <h3>
-          {data.place.showEstablished()}
-        </h3>
-        <h4>
-          Serving {data.place.cuisines}
-        </h4>
-        
-    </div>
-    <hr/>
-    <h2> Comments</h2>
-    {comments}
-  </main>
-</Def>
-  
-)
+        <main>
+          <div className="row">
+            <div className="col-sm-6">
+              <img src={data.place.pic} alt={data.place.name} />
+              <h3>
+                Located in {data.place.city}, {data.place.state}
+              </h3>
+            </div>
+            <div className="col-sm-6"></div>
+            <h2>Description</h2>
+            <h3>{data.place.showEstablished()}</h3>
+            <h4>Serving {data.place.cuisines}</h4>
+          </div>
+          <hr />
+          <h2> Comments</h2>
+          {comments}
+          <form action= {`/places/${data.place.id}/rant`}method="POST">
+            <input type="text" name="author" />
+            <input type="textarea" name="content"  />
+            <input type="Number" name="stars" />
+            <input type="checkbox" name="rant" />
+            <button type="submit">Submit</button>
+          </form>
+        </main>
+      </Def>
+    );
 }
 
 module.exports = show
